116ade2efdcde51ae95070605c6bf93a3d7d52c8
Brumix <brunompereira42@gmail.com>
2021-12-30 23:56:24 +0000

    service iniciated, Ger Request done

--- app/src/main/java/bruno/p/pereira/gpsindoorf/ui/beacons/BeaconsFragment.kt
+++ app/src/main/java/bruno/p/pereira/gpsindoorf/ui/beacons/BeaconsFragment.kt
@@ -1,11 +1,14 @@
 package bruno.p.pereira.gpsindoorf.ui.beacons
 
+import android.graphics.Color
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.animation.AnimationUtils
 import android.widget.Button
 import android.widget.ImageButton
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.navigation.Navigation
 import androidx.recyclerview.widget.LinearLayoutManager
@@ -14,8 +17,10 @@ import bruno.p.pereira.gpsindoorf.R
 import bruno.p.pereira.gpsindoorf.database.SQLiteHelper
 import bruno.p.pereira.gpsindoorf.databinding.FragmentBeaconsBinding
 import bruno.p.pereira.gpsindoorf.databinding.FragmentInfoBinding
+import bruno.p.pereira.gpsindoorf.services.HttpRequest
 import com.clj.fastble.BleManager
 import com.clj.fastble.data.BleScanState
+import java.sql.Time
 
 class BeaconsFragment : Fragment() {
 
@@ -52,8 +57,20 @@ class BeaconsFragment : Fragment() {
             adapter = _beaconAdapt
         }
 
+        val sync = binding.floatingSync
+        sync.setOnClickListener {
+            Toast.makeText(activity, "Syncing Information", Toast.LENGTH_LONG).show()
+            HttpRequest.startActionGET(this.requireContext(),20)
+            sync.animate().apply {
+                duration=2000
+                rotationBy(360f)
+            }.start()
+
+        }
+
         return root
     }
+
 
 
     override fun onDestroyView() {
