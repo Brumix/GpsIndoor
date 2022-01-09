8e180700cc5209c757a8b168a2856c1e300894ec
Brumix <brunompereira42@gmail.com>
2022-01-09 05:01:17 +0000

    back button apears but don't work

--- app/src/main/java/bruno/p/pereira/gpsindoorf/ui/graph/activity/GraphActivity.kt
+++ app/src/main/java/bruno/p/pereira/gpsindoorf/ui/graph/activity/GraphActivity.kt
@@ -1,10 +1,9 @@
 package bruno.p.pereira.gpsindoorf.ui.graph.activity
 
-import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle
-import android.view.MenuItem
 import android.view.View
 import androidx.appcompat.app.AlertDialog
+import androidx.appcompat.app.AppCompatActivity
 import bruno.p.pereira.gpsindoorf.R
 import bruno.p.pereira.gpsindoorf.graph.drawable.DrawableGraphView
 import bruno.p.pereira.gpsindoorf.graph.manager.ActionsManager
